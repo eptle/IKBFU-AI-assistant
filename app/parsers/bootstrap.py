7f3962428bb036cbb1bf3c4a236e70359d39ae09
Dmitry But <btlk04@gmail.com>
2026-05-08 23:07:01 +0200

    рефакторинг

--- app/parsers/bootstrap.py
+++
@@ -1,8 +0,0 @@
-import time
-from rich.progress import track
-
-for step in track(range(10), description="Обработка данных..."):
-    # Симулируем какую-то работу
-    time.sleep(0.5)
-
-print("[bold green]Готово![/bold green]")
